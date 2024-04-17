697b14cfe12e17e8d4a4de41ef1068b695079514
thenightmgod <l.dovidio19@gmail.com>
2024-04-17 19:38:13 +0200

    playing field inizia a esistere + qualche minchiata in giro player

--- CodexNaturalis/src/main/java/it/polimi/ingsw/Model/situaPlayer/Player.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/Model/situaPlayer/Player.java
@@ -1,4 +1,6 @@
 package it.polimi.ingsw.Model.situaPlayer;
+import it.polimi.ingsw.Model.situaCard.situaPlayableCard.PlayableCard;
+import it.polimi.ingsw.Model.situaCard.situaPlayableCard.ResourceCard;
 import it.polimi.ingsw.Model.situaCorner.Resources;
 import it.polimi.ingsw.Model.situaCorner.Objects;
 
@@ -9,12 +11,14 @@ public class Player {
     private int[] ResourceCounter;
     private int[] ObjectCounter;
 
+    private PlayingField Field; //todo il come inizializzarlo diocan
+
     public Player(String name, PlayerColor color) {
         Name = name;
         Color = color;
         PointsCounter = 0;
-        ResourceCounter = new int[4];
-        ObjectCounter = new int[3];
+        ResourceCounter = new int[4]; //raga scriviamo nei comments cosa è cosa in ogni posizione
+        ObjectCounter = new int[3]; //anche qua
     }
 
     public String getName() {
@@ -22,10 +26,10 @@ public class Player {
     }
     public Player(PlayerColor color) {
         Color = color;
-    }
+    } //raga che minchia è sta roba?
     public Player(int pointsCounter) {
         PointsCounter = pointsCounter;
-    }
+    } //stessa cosa
     public int getResourceCounter(Resources R){
         if(R==Resources.PLANT_KINGDOM)
             return ResourceCounter[0];
@@ -43,5 +47,15 @@ public class Player {
             return ObjectCounter[1];
         else
             return ObjectCounter[2];
+    }
+
+    public ResourceCard draw(Deck d){
+        //todo dopo il deck
+        return null;
+    }
+
+    public void placeCard(PlayableCard c, FB face){
+        Field.showFreePositions();
+
     }
 }
--- CodexNaturalis/src/main/java/it/polimi/ingsw/Model/situaPlayer/PlayingField.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/Model/situaPlayer/PlayingField.java
@@ -1,5 +1,22 @@
 package it.polimi.ingsw.Model.situaPlayer;
 
+import it.polimi.ingsw.Model.situaCard.situaPlayableCard.PlayableCard;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
 public class PlayingField {
-    // TODO
+
+    private HashMap<Position, PlayableCard> Field;
+    public PlayingField(){
+        Field = new HashMap<>();
+    }
+
+    public LinkedList<Position> showFreePositions(){
+        LinkedList<Position> p = new LinkedList<Position>();
+
+        //todo
+        //facciamo che ritorna un array di positions
+        return p;
+    }
 }
