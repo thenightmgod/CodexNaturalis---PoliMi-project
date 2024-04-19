0238a670a5dbe7339cf22cb783e44984b4c63e6c
cholent <hamin.jang@mail.polimi.it>
2024-04-19 16:34:33 +0200

    carte aggiustate davvero

--- CodexNaturalis/src/main/java/it/polimi/ingsw/Model/situaCard/situaPlayableCard/StartCard.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/Model/situaCard/situaPlayableCard/StartCard.java
@@ -1,7 +1,12 @@
 package it.polimi.ingsw.Model.situaCard.situaPlayableCard;
 
+import it.polimi.ingsw.Model.situaCorner.Corner;
+import it.polimi.ingsw.Model.situaCorner.Resources;
+
+import java.util.LinkedList;
+
 public class StartCard extends PlayableCard {
-    public StartCard(int id, int tiporesource) {
-        super(id, tiporesource);
+    public StartCard(int id, Resources R, LinkedList<Corner> Corners) {
+        super(id, R, Corners);
     }
 }
