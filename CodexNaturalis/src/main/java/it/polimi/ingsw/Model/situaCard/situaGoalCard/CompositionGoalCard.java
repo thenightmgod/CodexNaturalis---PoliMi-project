9e4ebe9a028b81320fd69319f505cc4caf924835
pietrogelati <pietrogel02@gmail.com>
2024-04-23 11:31:01 +0200

    JSON goal e fix di compgoal

--- CodexNaturalis/src/main/java/it/polimi/ingsw/Model/situaCard/situaGoalCard/CompositionGoalCard.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/Model/situaCard/situaGoalCard/CompositionGoalCard.java
@@ -1,15 +1,21 @@
 package it.polimi.ingsw.Model.situaCard.situaGoalCard;
 
+import it.polimi.ingsw.Model.situaCard.situaPlayableCard.CardColor;
 import  it.polimi.ingsw.Model.situaPlayer.Player;
 public class CompositionGoalCard extends GoalCard {
 
     private final Composition comp;
-    public CompositionGoalCard(int id, int points, Composition c) {
+    private final CardColor color;
+    public CompositionGoalCard(int id, int points, Composition c, CardColor color) {
         super(id,points);
         this.comp=c;
+        this.color=color;
     }
     public Composition getComp() {
         return comp;
+    }
+    public CardColor getColor() {
+        return color;
     }
 
     //non posso realizzarla fino a quando non so come funziona precisamente player ed il suo campo da gioco
