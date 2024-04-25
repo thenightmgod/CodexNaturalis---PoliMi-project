c0e066f199a6567a4ba58df127b4b1051b22d511
thenightmgod <l.dovidio19@gmail.com>
2024-04-25 12:32:13 +0200

    inizio test di deck

---
+++ CodexNaturalis/src/test/java/it/polimi/ingsw/Model/situaDeck/DeckTest.java
@@ -0,0 +1,9 @@
+package it.polimi.ingsw.Model.situaDeck;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeckTest {
+
+}
---
+++ CodexNaturalis/src/test/java/it/polimi/ingsw/Model/situaDeck/ResourceDeckTest.java
@@ -0,0 +1,15 @@
+package it.polimi.ingsw.Model.situaDeck;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ResourceDeckTest {
+
+    private static ResourceDeck deck;
+
+    @BeforeEach
+    void setUp() {
+        deck = new ResourceDeck();
+    }
+}
