ad3ab332d149ae151d924ffe1d201cbee04659ea
thenightmgod <l.dovidio19@gmail.com>
2024-05-01 16:25:39 +0200

    StartCard costruttore nuovo

--- CodexNaturalis/src/main/java/it/polimi/ingsw/Model/CardPackage/PlayableCardPackage/StartCard.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/Model/CardPackage/PlayableCardPackage/StartCard.java
@@ -17,15 +17,22 @@ import static it.polimi.ingsw.Model.CornerPackage.Resources.INSECT_KINGDOM;
  */
 public class StartCard extends PlayableCard {
 
+    private LinkedList<Corner> BackCorners;
     /**
      * Constructs a new StartCard.
      *
      * @param id          the specific identifier of the StartCard
      * @param R           the array of 4 positions, each representative of a resource (in the same order of the enum "Resources"),
      *                    containing in each position the number of that resource that the StartCard has in its back.
-     * @param Corners the list of four corners that each StartCard has
+     * @param BackCorners the list of four corners that each StartCard has in the back
+     * @param FrontCorners the list of four corners that each StartCard has in the front
      */
-    public StartCard(int id, boolean[] R, LinkedList<Corner> Corners) {
-        super(id, R, Corners);
+    public StartCard(int id, boolean[] R, LinkedList<Corner> BackCorners, LinkedList<Corner> FrontCorners) {
+        super(id, R, FrontCorners);
+        this.BackCorners = new LinkedList<>();
+        this.BackCorners.add(new Corner(BackCorners.get(0).getRes(), BackCorners.get(0).getOrientation()));
+        this.BackCorners.add(new Corner(BackCorners.get(1).getRes(), BackCorners.get(1).getOrientation()));
+        this.BackCorners.add(new Corner(BackCorners.get(2).getRes(), BackCorners.get(2).getOrientation()));
+        this.BackCorners.add(new Corner(BackCorners.get(3).getRes(), BackCorners.get(3).getOrientation()));
     }
 }
