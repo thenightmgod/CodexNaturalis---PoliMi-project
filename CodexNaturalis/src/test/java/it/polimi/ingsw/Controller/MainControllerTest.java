6946ea42f3eb48dee21546c8e4dd60209058075b
pietrogelati <pietrogel02@gmail.com>
2024-05-07 15:00:54 +0200

    test main c

--- CodexNaturalis/src/test/java/it/polimi/ingsw/Controller/MainControllerTest.java
+++ CodexNaturalis/src/test/java/it/polimi/ingsw/Controller/MainControllerTest.java
@@ -1,10 +1,31 @@
 package it.polimi.ingsw.Controller;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import it.polimi.ingsw.Exceptions.RoomFullException;
+import it.polimi.ingsw.Model.PlayerPackage.PlayerColor;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MainControllerTest {
-    MainController cicciopazzo = new MainController();
-    cicciopazzo
+
+    @Test
+    void Maincontrol(){
+        MainController game1 = new MainController();
+
+        game1.createGame("dalla", PlayerColor.BLUE, 3);
+        try{
+            game1.joinGame("venditti", PlayerColor.RED);
+        } catch (Exception e) {
+            if(e instanceof RoomFullException){
+
+            }
+        }
+
+
+
+
+
+    }
+
 }
