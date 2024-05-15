e9a52c6169d77ff514baa413862331f8dee94518
pietrogelati <pietrogel02@gmail.com>
2024-05-15 15:22:42 +0200

    test room va piu o meno pt 2

--- CodexNaturalis/src/test/java/it/polimi/ingsw/Model/RoomTest.java
+++ CodexNaturalis/src/test/java/it/polimi/ingsw/Model/RoomTest.java
@@ -3,6 +3,7 @@ package it.polimi.ingsw.Model;
 import static org.junit.jupiter.api.Assertions.*;
 
 import it.polimi.ingsw.Controller.GameController;
+import it.polimi.ingsw.Exceptions.WrongIndexException;
 import it.polimi.ingsw.Model.CardPackage.Card;
 import it.polimi.ingsw.Model.CardPackage.GoalCardPackage.*;
 import it.polimi.ingsw.Model.CardPackage.PlayableCardPackage.CardColor;
@@ -211,8 +212,10 @@ class RoomTest {
         beppe.commonGoals();
         beppe.getGame().setTwentyFlag();
         beppe.getGame().setLastRound();
+        try {
+            beppe.chooseGoalCard(beppe.getPlayers().getFirst(), 2);
+        } catch (WrongIndexException e) {
+            fail("Unexpected WrongIndexException: " + e.getMessage());
+        }
     }
 }
-    /**
-     *
-     */
