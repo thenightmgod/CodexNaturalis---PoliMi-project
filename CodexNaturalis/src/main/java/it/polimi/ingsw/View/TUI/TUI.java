a654747f56dd36e6990bb382b6f9484cd83b00c7
thenightmgod <l.dovidio19@gmail.com>
2024-05-17 10:41:18 +0200

    fix

--- CodexNaturalis/src/main/java/it/polimi/ingsw/View/TUI/TUI.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/View/TUI/TUI.java
@@ -1,5 +1,8 @@
 package it.polimi.ingsw.View.TUI;
 
+import it.polimi.ingsw.Exceptions.NameAlreadyTakenException;
+import it.polimi.ingsw.Exceptions.RoomFullException;
+import it.polimi.ingsw.Exceptions.RoomNotExistsException;
 import it.polimi.ingsw.Exceptions.WrongPlayersNumberException;
 import it.polimi.ingsw.Model.CardPackage.GoalCardPackage.GoalCard;
 import it.polimi.ingsw.Model.CardPackage.PlayableCardPackage.PlayableCard;
@@ -7,10 +10,12 @@ import it.polimi.ingsw.Model.CornerPackage.Objects;
 import it.polimi.ingsw.Model.PlayerPackage.Player;
 import it.polimi.ingsw.Model.PlayerPackage.PlayingField;
 import it.polimi.ingsw.Model.PlayerPackage.Position;
+import it.polimi.ingsw.Network.CommonClient;
 import it.polimi.ingsw.Network.RMI.RMIClient;
 import it.polimi.ingsw.Network.RMI.VirtualView;
 import it.polimi.ingsw.View.GameView;
 
+import javax.naming.Name;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -23,7 +28,7 @@ import java.util.concurrent.*;
 public class TUI implements GameView {
 
     Player Turn;
-    VirtualView client;
+    CommonClient client;
 
     @Override
     public void updatePoints(int points, String name) {
@@ -70,9 +75,9 @@ public class TUI implements GameView {
                 if(num<2 || num>4)
                     System.out.println("The number of players must be between 2 and 4!");
             } else{
-               num = 0;
-               goon = true;
-               break;
+                num = 0;
+                goon = true;
+                break;
             }
 
         } while(num < 2 || num > 4);
@@ -82,21 +87,42 @@ public class TUI implements GameView {
         } else{
             try{
                 name = getNickname();
-                //client va creato da qualche parte
+                client = chooseClient(name);
                 client.createGame(name, num); // è tutto nullo perchè così è inizializzata la view
 
-            } catch (RemoteException | WrongPlayersNumberException | NotBoundException e) {
-                throw new RuntimeException(e);
+            } catch (RemoteException e) {
+                System.out.println("an exception occurred while starting the client");
+            } catch (NotBoundException e){
+                System.out.print("NotBoundException occurred while initializing the client");
+            } catch (WrongPlayersNumberException e){
+                System.out.print("The number of players isn't supported");
             }
+
         }
 
         executor.shutdown();
 
     }
 
-    public void joinGame(){
+    public void joinGame() throws RoomFullException, RoomNotExistsException, NotBoundException, NameAlreadyTakenException, RemoteException {
+        boolean goon = false;
+        do {
+            try {
+                String name = getNickname();
+                this.client = chooseClient(name);
+                this.client.joinGame(name);
+                goon = true;
+            } catch (NameAlreadyTakenException e){
+                System.out.print("The name you chose is already taken, try again! UwU ;)");
+            }
+        } while(!goon);
+
+    }
+
+    public CommonClient chooseClient(String name){
         Scanner scan = new Scanner(System.in);
         String connection;
+
 
         System.out.println("What kind of connection would you like?");
 
@@ -108,21 +134,44 @@ public class TUI implements GameView {
                 try{
                     String nickname = getNickname();
                     client = new RMIClient(nickname);
-                } catch (NotBoundException | RemoteException e) {
-                    throw new RuntimeException(e);
+                    client.setView(this);
+                } catch (RemoteException e) {
+                    System.out.println("an exception occurred while starting the client");
+                } catch (NotBoundException e){
+                    System.out.print("NotBoundException occurred while initializing the client");
                 }
             }
-        } while();
+
+            //fare socketclient inizializzazione quando c'è, startarlo dopo e gestire eccezioni
+
+            /*if(connection.equals("1")){
+                try{
+                    String nickname = getNickname();
+                    // client = new SocketClient()
+                    client.setView(this);
+                } catch{(NotBoundException | RemoteException e) {
+                    System.out.println("an exception occured while starting the client");
+                }
+                }
+            }*/
+
+        } while(!connection.equals("0") && !connection.equals("1"));
+
+        return client;
     }
+
 
     public String getNickname(){
         String nickname;
-        try{
+        boolean goon = false;
+        try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
             nickname = reader.readLine();
+            goon = true;
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+
         return nickname;
     }
 
@@ -140,4 +189,3 @@ public class TUI implements GameView {
     }
 
 }
-
