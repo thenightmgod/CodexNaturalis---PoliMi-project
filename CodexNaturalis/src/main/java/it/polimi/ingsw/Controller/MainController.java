7a5cba67e76c0b385e7ff1eaf34261793e53221c
thenightmgod <l.dovidio19@gmail.com>
2024-05-20 13:49:30 +0200

    main controller meglio in teoria showexception funge

--- CodexNaturalis/src/main/java/it/polimi/ingsw/Controller/MainController.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/Controller/MainController.java
@@ -3,11 +3,14 @@ package it.polimi.ingsw.Controller;
 import it.polimi.ingsw.Exceptions.*;
 import it.polimi.ingsw.Model.PlayerPackage.Player;
 import it.polimi.ingsw.Model.PlayerPackage.PlayerColor;
+import it.polimi.ingsw.Model.RoomPackage.ObserverManager;
 import it.polimi.ingsw.Network.VirtualView;
 
 import java.rmi.RemoteException;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.stream.Collectors;
 
 //questo controlla game multipli
 public class MainController {
@@ -51,26 +54,38 @@ public class MainController {
         //RoomNotExistsException
         int i;
         if(this.controllers.isEmpty()){
-            throw new RoomNotExistsException("there isn't a game to join, create one");
+            ObserverManager Garfield = new ObserverManager();
+            Garfield.addObserver(client, Name);
+            Garfield.showException("RoomNotExistsException", Name);
+            Garfield.removeObserver(client, Name);
         }
         else {
-            for(Player p : this.controllers.getLast().getPlayers()){
+            String boh = controllers.stream().map(GameController::getPlayers).flatMap(Collection::stream).map(Player::getName).findAny().orElse("");
+            if(!boh.isEmpty()) {
+                ObserverManager Garfield = new ObserverManager();
+                Garfield.addObserver(client, Name);
+                Garfield.showException("NameAlreadyTakenException", Name);
+                Garfield.removeObserver(client, Name);
+            }
+        }
+            /*for(Player p : this.controllers.getLast().getPlayers()){
                 if(p.getName() == Name)
                     //non funzionerà mai
-                    this.controllers.getLast().getGame().getObserverManager().showException("NameAlreadyTakenException", this.controllers.getLast().getPlayerByName(p.getName()).getName());
-            }
-            if (this.controllers.getLast().getHowManyPlayers() == this.controllers.getLast().getNumPlayers())
-                //non funziona perchè il player non è nella room, fare da tui
-                i = 5;
-            else {
-                switch(this.controllers.getLast().getNumPlayers()){
-                    case 1 ->
-                            this.controllers.getLast().addPlayer(Name, PlayerColor.YELLOW, client);
-                    case 2 ->
-                            this.controllers.getLast().addPlayer(Name, PlayerColor.BLUE, client);
-                    case 3 ->
-                            this.controllers.getLast().addPlayer(Name, PlayerColor.GREEN, client);
-                }
+                    this.controllers.getLast().getGame().getObserverManager().showException("NameAlreadyTakenException", this.controllers.getLast().getPlayerByName(p.getName()).getName());*/
+        if (this.controllers.getLast().getHowManyPlayers() == this.controllers.getLast().getNumPlayers()) {
+            ObserverManager Garfield = new ObserverManager();
+            Garfield.addObserver(client, Name);
+            Garfield.showException("RoomFullException", Name);
+            Garfield.removeObserver(client, Name);
+        }
+        else {
+            switch(this.controllers.getLast().getNumPlayers()){
+                case 1 ->
+                        this.controllers.getLast().addPlayer(Name, PlayerColor.YELLOW, client);
+                case 2 ->
+                        this.controllers.getLast().addPlayer(Name, PlayerColor.BLUE, client);
+                case 3 ->
+                        this.controllers.getLast().addPlayer(Name, PlayerColor.GREEN, client);
             }
         }
         return controllers.getLast();
