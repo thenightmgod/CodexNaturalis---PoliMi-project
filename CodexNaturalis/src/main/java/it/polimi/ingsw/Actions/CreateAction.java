e568d7d79051704f4d37a1ffa6537c0014dc66bd
pietrogelati <pietrogel02@gmail.com>
2024-05-23 17:00:37 +0200

    modifiche ad actions 20098

--- CodexNaturalis/src/main/java/it/polimi/ingsw/Actions/CreateAction.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/Actions/CreateAction.java
@@ -3,9 +3,11 @@ package it.polimi.ingsw.Actions;
 import it.polimi.ingsw.Controller.GameController;
 import it.polimi.ingsw.Controller.MainController;
 import it.polimi.ingsw.Exceptions.WrongPlayersNumberException;
+import it.polimi.ingsw.Model.PlayerPackage.Player;
 import it.polimi.ingsw.Network.VirtualView;
 
 import java.rmi.RemoteException;
+import java.util.Map;
 
 public class CreateAction extends Actions{
 
@@ -21,7 +23,16 @@ public class CreateAction extends Actions{
 
     @Override
     public void executor() throws RemoteException {
-        GameController c = this.getManager().createGame(name, i, getView());
-        getManager().getControllersPerGame().put(c.getRoomId(), c);
+        int k = -1;
+        for(Map.Entry<Integer, GameController> entry : getManager().getControllersPerGame().entrySet()){
+            if(entry.getValue().getPlayers().stream().map(Player::getName).toList().contains(getView().getName())){
+                k = entry.getKey();
+            }
+        }
+        if(k != -1){
+            GameController controller = this.getManager().createGame(name, i, getView());
+            getManager().getControllersPerGame().put(controller.getRoomId(), controller);
+        }
     }
 }
+
