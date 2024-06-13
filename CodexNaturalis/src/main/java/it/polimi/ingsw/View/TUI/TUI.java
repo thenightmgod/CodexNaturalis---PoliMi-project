aa06ac28a3340a48cb08e6ef25c6893729b8f66b
cholent <162326221+cholent@users.noreply.github.com>
2024-06-13 17:12:41 +0200

    fix tui2

--- CodexNaturalis/src/main/java/it/polimi/ingsw/View/TUI/TUI.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/View/TUI/TUI.java
@@ -14,6 +14,7 @@ import it.polimi.ingsw.Model.PlayerPackage.Position;
 import it.polimi.ingsw.Network.CommonClient;
 import it.polimi.ingsw.Network.RMI.RMIClient;
 //import it.polimi.ingsw.Network.Socket.SocketClient;
+import it.polimi.ingsw.Network.Socket.SocketClient;
 import it.polimi.ingsw.View.GameView;
 
 import java.io.BufferedReader;
@@ -367,9 +368,9 @@ public class TUI implements GameView {
                     System.out.print("NotBoundException occurred while initializing the client");
                 }
             }else {
-                /*client = new SocketClient(name);
+                client = new SocketClient(name);
                 client.setView(this);
-                connectionType = true;*/
+                connectionType = true;
             }
 
         } while(!connection.equals("0") && !connection.equals("1"));
