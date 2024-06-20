edf5a0b433229e2635eb83dc49313d94a64e6882
cholent <162326221+cholent@users.noreply.github.com>
2024-06-20 17:50:27 +0200

    scena con pepo pushing p 20073

--- CodexNaturalis/src/main/java/it/polimi/ingsw/View/GUI/GUIController/TurnController.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/View/GUI/GUIController/TurnController.java
@@ -31,6 +31,12 @@ public class TurnController extends GUIController{
     @FXML
     private Label messageLabel;
     @FXML
+    private Label myResource;
+    @FXML
+    private Label myGold;
+    @FXML
+    private Label myGoal;
+    @FXML
     private HBox label_button_box;
     @FXML
     private Button myPointsButton;
@@ -133,6 +139,8 @@ public class TurnController extends GUIController{
     private void loadResourceBox() {
         resourceBox.setPrefHeight(160.0);
         resourceBox.setPrefWidth(492.0);
+        myResource.setText("DRAWABLE RESOURCECARDS");
+        myResource.setVisible(true);
         double imageViewWidth = (resourceBox.getPrefWidth()-45) / 3.0;
         try {
             for (int i =resourcedeck.size()-1 ; i >=0 ; i--) {
@@ -157,6 +165,8 @@ public class TurnController extends GUIController{
     private void loadGoldBox() {
         goldBox.setPrefHeight(160.0);
         goldBox.setPrefWidth(492.0);
+        myGold.setText("DRAWABLE GOLDCARDS");
+        myGold.setVisible(true);
         double imageViewWidth = (goldBox.getPrefWidth()-45) / 3.0;
 
         try {
@@ -182,6 +192,8 @@ public class TurnController extends GUIController{
     private void loadGoalBox() {
         goalsBox.setPrefHeight(160.0);
         goalsBox.setPrefWidth(492.0);
+        myGoal.setText("GOALCARDS");
+        myGoal.setVisible(true);
         double imageViewWidth = (goalsBox.getPrefWidth()-45) / 3.0;
         try {
             for (int i = commongoals.size()-1 ; i >=0 ; i--) {
